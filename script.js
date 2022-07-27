f7517061f2c7aa353aba1b6fa362c44d2dfe2fc7
Ivan Napolskykh <38167469+vainia@users.noreply.github.com>
2022-07-27 18:11:33 +0200

    Finally the base attributes filtering!

--- script.js
+++ script.js
@@ -1,89 +1,124 @@
-const placeholder = document.querySelector("#__next > main > div:last-child > div:first-child > div")
-
-const stepnSortParams = {
-    lowToHigh: "order=1001",
-    latest: "order=1002",
-    hightToLow: "order=2002"
-}
-
+let paramsDefinedByUser = "&order=2001&chain=103&type=&gType=&quality=&level=0&breed=0"
 const stepnApi = {
-    // TODO: fix page query param - each call to this endpoint returns data for next data set disregarding the page param
-    sneakersData: (pageIdx, sessionId) => `https://api.stepn.com/run/orderlist?${stepnSortParams.hightToLow}&chain=103&refresh=false&page=${pageIdx}&type=&gType=&quality=&level=0&bread=0&sessionID=${sessionId}`,
-    sneakerPage: id => `https://m.stepn.com/order/${id}`,
-    sneakerDetails: (orderId, sessionId) => `https://api.stepn.com/run/orderdata?orderId=${orderId}&sessionID=${sessionId}`
-    // ->data
-    //          ->attrs [0-Efficiency, 1-Luck, 2-Comfort, 3-Resilience] - All bases, [4-7] - All points assigned respectively
-    //          ->breed (0-7)
-    //          ->holes [0-4]
-    //              ->type (1-Efficiency, 2-Luck, 3-Comfort, 4-Resilience)
-    //              ->quality (-1, 0, ...)
-    //          ->level (0-30)
-    //          ->price (?)
-    //          ->type (1-Walker, 2-Jogger, 3-Runner, 4-Trainer) - Not certain
+    orderList: (pageIdx, sessionId) => `https://api.stepn.com/run/orderlist?refresh=false&page=${pageIdx}&sessionID=${sessionId}&simulated=true${paramsDefinedByUser}`,
+    orderData: (orderId, sessionId) => `https://api.stepn.com/run/orderdata?orderId=${orderId}&sessionID=${sessionId}&simulated=true`
 }
-
-let resultSneaker = null
 
 const getSessionId = () => new Promise((resolve, reject) => window.cookieStore.get("sessionID").then(
     data => resolve(encodeURIComponent(data.value))
 ))
 
-const findSneaker = (pageIdx, sessionId) => fetch(stepnApi.sneakersData(pageIdx, sessionId), {
-    method: "GET",
-    mode: "cors",
-    credentials: "include"
-}).then((e) => e.json()).then(async (e) => {
-    if (resultSneaker || e.data.length === 0) {
-        assignInnerHtmlById(inputIds.filterResultOutput, "No sneaker found")
-        return true
-    }
-    // const targetSneakerIdLength = dataSelectors.targetSneakerIdLength()
-    for (let entry of e.data) {
-        assignInnerHtmlById(inputIds.iteratorCurrentPage, `Current Page: ${pageIdx} \n Price: ${formatPrice(entry.sellPrice)}`)
-        // const sneakerId = entry.otd.toString()
-        // console.log({ a: sneakerId.length, b: targetSneakerIdLength })
-        // if (sneakerId.length <= targetSneakerIdLength) {
-        //     resultSneaker = entry
-        //     assignInnerHtmlById(inputIds.filterResultOutput, prepareResultHtml(entry))
-        //     return true
-        // }
-        const orderId = entry.id.toString()
-        
-        // Sneaker is found
-        if (resultSneaker) {
-            // assignInnerHtmlById(inputIds.filterResultOutput, "Sneaker is found")
-            return true
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms))
+const formatPrice = (price) => parseFloat(price / 1000000).toFixed(4)
+const assignInnerHtmlById = (id, html) => document.getElementById(id).innerHTML = html
+
+let limitResultsCount = 5
+const sneakersPerPage = 60
+
+const findSneaker = (pageIdx, sessionId) => new Promise((resolve, reject) => { 
+    fetch(stepnApi.orderList(pageIdx, sessionId), {
+        method: "GET",
+        mode: "cors",
+        credentials: "include"
+    }).then((e) => e.json()).then(async (e) => {
+        if (!e.data || e.data.length === 0) {
+            reject(false)
+            throw new Error("Order list request error")
         }
 
-        // Await necessary amount of ms to avoid geting blacklisted IP
-        await timeout(0.5 * 1000)
-        fetch(stepnApi.sneakerDetails(orderId, sessionId), {
-            method: "GET",
-            mode: "cors",
-            credentials: "include"
-        }).then((e) => e.json()).then((e) => {
-            if (resultSneaker || !e.data) {
-                // assignInnerHtmlById(inputIds.filterResultOutput, "Processing...")
-                return true
+        let currentIterationSet = (pageIdx + 1) * sneakersPerPage
+        let currentOrderIteration = currentIterationSet - sneakersPerPage
+        for (let order of e.data) {
+            currentOrderIteration++
+            if (targetOrderList.length >= limitResultsCount) {
+                resolve(true)
+                return
             }
-            if (e.data.attrs[0] > 9 * 10 && e.data.attrs[3] > 9 * 10) {
-                resultSneaker = entry
-                debugger
-                assignInnerHtmlById(inputIds.filterResultOutput, prepareResultHtml(entry))
-                return true
+
+            const minEffBase = dataSelectors.filterMinEffBase()
+            const minLuckBase = dataSelectors.filterMinLuckBase()
+            const minComBase = dataSelectors.filterMinComBase()
+            const minResBase = dataSelectors.filterMinResBase()
+
+            assignInnerHtmlById(inputIds.iteratorCurrentPage, `Sneakers iterated: ${currentOrderIteration}/${currentIterationSet}<br />Price: ${formatPrice(order.sellPrice)}`)
+
+            // Await necessary amount of ms to avoid getting blacklisted IP
+            await timeout(0.25 * 1000)
+            
+            // ->data[]->attrs [0-Efficiency, 1-Luck, 2-Comfort, 3-Resilience] - All bases, [4-7] - All points assigned respectively
+            // ->data[]->breed (0-7)
+            // ->data[]->holes[0-4]->type (1-Efficiency, 2-Luck, 3-Comfort, 4-Resilience)
+            // ->data[]->holes[0-4]->quality (-1, 0, ...)
+            // ->data[]->level (0-30)
+            // ->data[]->type (1-Walker, 2-Jogger, 3-Runner, 4-Trainer)
+            const orderData = await getOrderData(order.id.toString(), sessionId)
+            
+            // Sneaker got sold
+            if (!orderData) { 
+                assignInnerHtmlById(inputIds.iteratorCurrentPage, `Order for iteration #${currentOrderIteration} got sold`)
+                continue
             }
-        })
-    }
-    return false
+
+            // Attributes are not present when shoebox is encountered
+            if (!orderData.attrs) {
+                continue
+            }
+    
+            const orderBaseEff = orderData.attrs[0] / 10
+            const orderBaseLuck = orderData.attrs[1] / 10
+            const orderBaseCom = orderData.attrs[2] / 10
+            const orderBaseRes = orderData.attrs[3] / 10
+    
+            if (minEffBase <= orderBaseEff && minLuckBase <= orderBaseLuck && minComBase <= orderBaseCom && minResBase <= orderBaseRes) {
+                targetOrderList.push(order)
+                assignInnerHtmlById(inputIds.filterResultOutput, `Sneakers found: ${targetOrderList.length}/${limitResultsCount}`)
+                if (targetOrderList.length >= limitResultsCount) {
+                    resolve(true)
+                    return
+                }
+            }
+        }
+        resolve(false)
+    })
 })
 
-const timeout = ms => new Promise(resolve => setTimeout(resolve, ms))
+const getOrderData = async (orderId, sessionId) => new Promise((resolve, reject) => { 
+    fetch(stepnApi.orderData(orderId, sessionId), {
+        method: "GET",
+        mode: "cors",
+        credentials: "include"
+    }).then((e) => e.json()).then((e) => {
+        if (!e.data || e.data.length === 0) {
+            resolve(null)
+            return
+        }
+        resolve(e.data)
+    })
+})
 
-const {fetch: origFetch} = window;
+// Intercept the order list request and replace it with the target order list
+let targetOrderList = []
+const { fetch: origFetch } = window
 window.fetch = async (...args) => {
     return new Promise(async (resolve, reject) => {
         const response = await origFetch(...args)
-        if(!resultSneaker || !args || !args[0] || args[0].indexOf(stepnSortParams.latest) < 0) {
+
+        const swapOrderList = args[0].indexOf("simulated") === -1 && args[0].indexOf("orderlist") !== -1
+
+        // If new order list is requested by user grab native filter parameters to combine with enhanced filter
+        if (swapOrderList) {
+            var url = new URL(args[0])
+
+            paramsDefinedByUser = "&order=" + url.searchParams.get("order")
+                + "&chain=" + url.searchParams.get("chain")
+                + "&type=" + url.searchParams.get("type")
+                + "&gType=" + url.searchParams.get("gType")
+                + "&quality=" + url.searchParams.get("quality")
+                + "&level=" + url.searchParams.get("level")
+                + "&breed=" + url.searchParams.get("breed")
+        }
+
+        if (!swapOrderList || targetOrderList.length === 0 || !args || !args[0]) {
             resolve(response)
         }
 
@@ -92,12 +127,10 @@ window.fetch = async (...args) => {
             .json()
             .then(body => {
                 response.json = () => {
-                    body.data = [
-                        resultSneaker
-                    ]
+                    body.data = targetOrderList
                     return body
                 }
-                resolve(response)  
+                resolve(response)
             })
             .catch((error) => {
                 reject(response)
@@ -105,46 +138,88 @@ window.fetch = async (...args) => {
     })
 }
 
-const formatPrice = (price) => parseFloat(price/1000000).toFixed(2)
-
-const prepareResultHtml = (entry) => `#${entry.otd} - sort by "Latest" to see the sneaker`
-
 const applyEnhancedFilters = async () => {
-    let intervalSeconds = 1
-    let pageIndex = 0
-    resultSneaker = null
+    targetOrderList = []
+    limitResultsCount = 5
 
+    let pageIndex = 0
     assignInnerHtmlById(inputIds.filterResultOutput, "Searching...")
 
-    const sessionId = await getSessionId()
-    // TODO: Fix this interval to accomodate timeouts
-    // const genesisSearchInterval = setInterval(() => {
-        findSneaker(pageIndex++, sessionId).then((finished) => finished) // && clearInterval(genesisSearchInterval))
-    // }, intervalSeconds * 1000)
+    while (true) {
+        await timeout(0.25 * 1000)
+        const sessionId = await getSessionId()
+        const isFound = await findSneaker(pageIndex++, sessionId)
+        if (isFound) {
+            renderTargetOrderList()
+            return
+        }
+    }
 }
 
-const assignInnerHtmlById = (id, html) => document.getElementById(id).innerHTML = html
+const stopFilterSearch = () => {
+    limitResultsCount = targetOrderList.length
+}
+
+const renderTargetOrderList = async () => {
+    if (limitResultsCount === 0) {
+        return
+    }
+
+    const sortDropdownButton = document.querySelector("#headlessui-listbox-button-\\:r3\\:")
+    sortDropdownButton.innerText = "Enchanced Filtering"
+    sortDropdownButton.dispatchEvent(new Event('click', { bubbles: true }));
+    await timeout(0.25 * 1000)
+    const unselectedSortOption = document.querySelector("li[id^=headlessui-listbox-option]:not([aria-selected])")
+    unselectedSortOption.dispatchEvent(new Event('click', { bubbles: true }));
+}
 
 const inputIds = {
     filterResultOutput: "filter-result-output",
-    limitSneakerIdDigits: "filter-limit-sneaker-id-digits",
+    minEffBase: "filter-min-eff-base",
+    minLuckBase: "filter-min-luck-base",
+    minComBase: "filter-min-com-base",
+    minResBase: "filter-min-res-base",
     iteratorCurrentPage: "filter-result-current-page"
 }
 
 const dataSelectors = {
-    targetSneakerIdLength: () => parseInt(document.getElementById(inputIds.limitSneakerIdDigits).value || '0'),
+    filterMinEffBase: () => parseInt(document.getElementById(inputIds.minEffBase).value || '0'),
+    filterMinLuckBase: () => parseInt(document.getElementById(inputIds.minLuckBase).value || '0'),
+    filterMinComBase: () => parseInt(document.getElementById(inputIds.minComBase).value || '0'),
+    filterMinResBase: () => parseInt(document.getElementById(inputIds.minResBase).value || '0'),
 }
 
+const copyWalletToBuffer = () => {
+    var copyText = document.getElementById("stepn-wallet-address")
+    copyText.select()
+    copyText.setSelectionRange(0, 99999)
+    navigator.clipboard.writeText(copyText.value)
+  }
+
 const enhancedFiltersFormString = `<div id="stepn-enhanced-filters-by-inapolsky" class="p-5">
-    <h1 class="mb-5 border-b-[4px] border-green-400"><b>Enhanced Filtering</b></h1>
-    <label for="${inputIds.limitSneakerIdDigits}" class="capitalize">Limit digits in sneaker\'s ID</label>
+    <h1 class="mb-5 border-b-[4px] border-green-400">
+        <b>Enhanced Filtering</b>
+        <hr/>
+        <i>Works With Native Filters</i>
+        <hr/>
+        <i>By inapolsky</i>
+        <input type="text" value="2stFvNdELDQ56C6vD71Lt3jKN9Th4r7cfkpJchwyxJ6x" id="stepn-wallet-address" style="text-overflow: ellipsis;" class="mt-4"/>
+        <button  class="w-full h-[34px] border bg-white text-sm capitalize m-auto mb-4" onclick="copyWalletToBuffer()">
+            Donate (GST|GMT|SOL)
+        </button>
+    </h1>
+    ${['minEffBase', 'minLuckBase', 'minComBase', 'minResBase'].map(ft =>
+    `<label for="${inputIds[ft]}" class="capitalize">${ft.replace("min", "Min. ").replace("Base", " Base")}</label>
     <div class="range-verbose flex">
-        <input type="range" min="1" max="9" placeholder="Enter count of digits" name="${inputIds.limitSneakerIdDigits}" id="${inputIds.limitSneakerIdDigits}"
+        <input type="range" min="1" max="112" name="${inputIds[ft]}" id="${inputIds[ft]}"
             value="9" class="w-full h-[34px] border bg-white text-sm capitalize m-auto mb-4" oninput="this.nextElementSibling.value = this.value" />
         <output class="px-5 text-lg font-bold">9</output>
-    </div>
+    </div>`).join("")}
     <button class="w-full h-[34px] border bg-white text-sm capitalize m-auto mb-4" onclick="applyEnhancedFilters();">
         Apply Filters
+    </button>
+    <button class="w-full h-[34px] border bg-white text-sm capitalize m-auto mb-4" onclick="stopFilterSearch();">
+        Stop Filter Search
     </button>
     <div class="flex-column">
         <div class="text-lg 2xl:text-base 2xl:leading-5" id="${inputIds.iteratorCurrentPage}"></div>
@@ -152,4 +227,5 @@ const enhancedFiltersFormString = `<div id="stepn-enhanced-filters-by-inapolsky"
     </div>
 </div>`
 
-placeholder.innerHTML = placeholder.innerHTML + enhancedFiltersFormString
+const placeholder = document.querySelector("#__next > main > div:last-child > div:first-child > div")
+placeholder.insertAdjacentHTML("afterend", enhancedFiltersFormString)
